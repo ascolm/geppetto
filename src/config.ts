646ceeb703346e5283aa422f0f0d6b93400d2fd5
ascolm <mertascioglu@gmail.com>
2021-05-12 15:49:34 +0300

    build(config): add amazon urls to config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
 const config = {
   goodreadsBestOfUrl: 'https://www.goodreads.com/choiceawards/best-books-2020',
-  goodreadsBaseUrl: 'https://www.goodreads.com'
+  goodreadsBaseUrl: 'https://www.goodreads.com',
+  chromeExecutablePath: 'zz/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+  amazonQueryFormat: 'https://www.amazon.com/s?k={{SEARCHPLACEHOLDER}}&i=stripbooks-intl-ship',  // {{SEARCHPLACEHOLDER}} gets replaced with search term in the associated helper function
+  amazonBaseUrl: 'https://www.amazon.com',
+  amazonPrintSubDir: '/dp/'
+
 };
 
 export default config;
